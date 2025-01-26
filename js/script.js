41432aa8aff3b227cf83530ced4d1269956af131
Sebastian Ziedalski <sebastian.ziedalski@gmail.com>
2025-01-27 00:16:23 +0100

    Add buttons

--- js/script.js
+++ js/script.js
@@ -41,7 +41,34 @@ printMessage('Twój ruch to: ' + argPlayerMove);
 console.log('moves:', argComputerMove, argPlayerMove);
 */
 
-let randomNumber = Math.floor(Math.random() * 3 + 1);
+function playGame(playerInput){
+    // clearMessages();
+    let randomNumber = Math.floor(Math.random() * 3 + 1);
+    console.log('Wylosowana liczba to: ' + randomNumber);
+    let argComputerMove = getMoveName(randomNumber);
+
+    //let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+    console.log('Gracz wpisał: ' + playerInput);
+    let argPlayerMove = getMoveName(playerInput);
+
+    console.log('Wybór komputer i gracz:', argComputerMove, argPlayerMove);
+    printMessage(displayResult(argComputerMove, argPlayerMove));
+}
+
+document.getElementById('play-rock').addEventListener('click', function(){
+    playGame(1);
+});
+document.getElementById('play-paper').addEventListener('click', function(){
+    playGame(2);
+});
+document.getElementById('play-scissors').addEventListener('click', function(){
+    playGame(3);
+});
+
+
+
+
+/* let randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('Wylosowana liczba to: ' + randomNumber);
 let argComputerMove = getMoveName(randomNumber);
 
@@ -51,3 +78,4 @@ let argPlayerMove = getMoveName(playerInput);
 
 console.log('Wybór komputer i gracz:', argComputerMove, argPlayerMove);
 printMessage(displayResult(argComputerMove, argPlayerMove));
+*/
