67bc97bc90dd8e58945088990da3212c7f85a1b8
Sebastian Ziedalski <sebastian.ziedalski@gmail.com>
2025-02-01 23:18:09 +0100

    change function to const

--- js/script.js
+++ js/script.js
@@ -1,29 +1,5 @@
-function playGame(playerInput){
-    // clearMessages();
-    let randomNumber = Math.floor(Math.random() * 3 + 1);
-    console.log('Wylosowana liczba to: ' + randomNumber);
-    let argComputerMove = getMoveName(randomNumber);
-
-    //let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-    console.log('Gracz wpisał: ' + playerInput);
-    let argPlayerMove = getMoveName(playerInput);
-
-    console.log('Wybór komputer i gracz:', argComputerMove, argPlayerMove);
-    printMessage(displayResult(argComputerMove, argPlayerMove));
-}
-
-document.getElementById('play-rock').addEventListener('click', function(){
-    playGame(1);
-});
-document.getElementById('play-paper').addEventListener('click', function(){
-    playGame(2);
-});
-document.getElementById('play-scissors').addEventListener('click', function(){
-    playGame(3);
-});
-
-
-function getMoveName(argMoveId){
+{
+const getMoveName = (argMoveId) => {
     if(argMoveId == 1){
       return 'kamień';
     } else if(argMoveId == 2){
@@ -36,7 +12,7 @@ function getMoveName(argMoveId){
     }
 }
 
-function displayResult(argComputerMove, argPlayerMove){
+const displayResult = (argComputerMove, argPlayerMove) =>{
     printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
     if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
       return('Ty wygrywasz!');
@@ -57,4 +33,27 @@ function displayResult(argComputerMove, argPlayerMove){
     }
 }
 
-// funkcje się znajdowały w pliku functions.js, ale zostały przeniesione do pliku script.js, ponieważ tam są wywoływane. 
+const playGame = (playerInput) => {
+    // clearMessages();
+    const randomNumber = Math.floor(Math.random() * 3 + 1);
+    console.log('Wylosowana liczba to: ' + randomNumber);
+    const argComputerMove = getMoveName(randomNumber);
+
+    //let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+    console.log('Gracz wpisał: ' + playerInput);
+    const argPlayerMove = getMoveName(playerInput);
+
+    console.log('Wybór komputer i gracz:', argComputerMove, argPlayerMove);
+    printMessage(displayResult(argComputerMove, argPlayerMove));
+}
+
+document.getElementById('play-rock').addEventListener('click', () => {
+    playGame(1);
+});
+document.getElementById('play-paper').addEventListener('click', () => {
+    playGame(2);
+});
+document.getElementById('play-scissors').addEventListener('click', () => {
+    playGame(3);
+});
+}
